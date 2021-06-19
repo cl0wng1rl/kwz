5f09b23a4bf32b18ec834c860ac1e1a289d6f3dd
Gabriel Barker <gabrielbarker@gmail.com>
2021-06-19 01:13:48 +0100

    TEST the `Request` class

---
+++ src/test/Request.test.ts
@@ -0,0 +1,40 @@
+import Request from "../main/Request";
+
+const responseOnFn = jest.fn((_: string, fn: Function) => fn());
+const requestOnFn = jest.fn((_: string, fn: Function) => fn());
+const requestEndFn = jest.fn();
+
+const mockResponse = jest.fn(() => ({ on: responseOnFn }));
+const mockRequestFunction = jest.fn((options, cb) => {
+  cb(mockResponse());
+  return { on: requestOnFn, end: requestEndFn };
+});
+
+jest.mock("https", () => ({
+  request: (options: any, callBack: any) => mockRequestFunction(options, callBack),
+}));
+
+const options = { host: "host", path: "path" };
+
+const expectFunctionsAreCalledCorrectly = () => {
+  expect(mockRequestFunction).toBeCalled();
+  expect(mockRequestFunction).toBeCalledWith(
+    expect.objectContaining(options), 
+    expect.any(Function)
+  );
+  expect(responseOnFn).toBeCalledWith("data", expect.any(Function));
+  expect(responseOnFn).toBeCalledWith("end", expect.any(Function));
+  expect(requestOnFn).toBeCalledWith("error", expect.any(Function));
+  expect(requestEndFn).toBeCalled();
+};
+
+describe("Request", () => {
+  it("calls 'https' request correctly", () => {
+    // Given
+    const request = new Request(options);
+    // When
+    request.get();
+    // Then
+    expectFunctionsAreCalledCorrectly();
+  });
+});
