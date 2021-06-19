1ceed1dabab8f69e0fe7645508146c4f9b48b7f7
Gabriel Barker <gabrielbarker@gmail.com>
2021-06-19 19:14:22 +0100

    USE `QuestionFactory` in `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+import Request from './main/Request';
+import QuestionFactory from './main/QuestionFactory';
+var options = {
+  host: 'opentdb.com',
+  path: '/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+};
+const request = new Request(options);
+new QuestionFactory(request).getQuestions().then(questions => {
+  questions.forEach(q => console.log(q));
+});
