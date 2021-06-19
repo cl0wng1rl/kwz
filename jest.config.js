a98ceeff72808b25496ebcfa5644b9c65445b69e
Gabriel Barker <gabrielbarker@gmail.com>
2021-06-19 01:13:08 +0100

    CONFIGURE jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ["json", "lcov", "text", "clover"],
+};
