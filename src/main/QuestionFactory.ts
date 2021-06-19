c23610e0f3b3cd1ac047a3e424ca019b1ca17f69
Gabriel Barker <gabrielbarker@gmail.com>
2021-06-19 19:13:54 +0100

    ADD `QuestionFactory` class

---
+++ src/main/QuestionFactory.ts
@@ -0,0 +1,26 @@
+import Request from "./Request";
+import Question from "./Question";
+
+class QuestionFactory {
+  private static QUESTION = "question";
+  private static CORRECT_ANSWER = "correct_answer";
+  private static INCORRECT_ANSWERS = "incorrect_answers";
+
+  constructor(private request: Request) {}
+
+  public async getQuestions(): Promise<Question[]> {
+    const result = await this.request.get();
+    const questionsJSON = JSON.parse(result).results;
+    return questionsJSON.map(this.getQuestion);
+  }
+
+  private getQuestion(questionJSON: any): Question {
+    return new Question(
+      questionJSON[QuestionFactory.QUESTION],
+      questionJSON[QuestionFactory.CORRECT_ANSWER],
+      questionJSON[QuestionFactory.INCORRECT_ANSWERS]
+    );
+  }
+}
+
+export default QuestionFactory;
