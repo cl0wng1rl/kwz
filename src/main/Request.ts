1376f39d5aea41bb9867832d9753d8af07609c25
Gabriel Barker <gabrielbarker@gmail.com>
2021-06-19 01:13:36 +0100

    ADD `Request` class
    This class wraps the https request

---
+++ src/main/Request.ts
@@ -0,0 +1,23 @@
+import { request } from "https";
+import { IncomingMessage } from "http";
+
+type Options = { host: string; path: string };
+
+class Request {
+  constructor(private options: Options) {}
+
+  public async get(): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const rq = request(this.options, (response: IncomingMessage) => {
+        let data = "";
+        response.on("data", (chunk) => (data += chunk));
+        response.on("end", () => resolve(data));
+      });
+
+      rq.on("error", (e) => reject(e));
+      rq.end();
+    });
+  }
+}
+
+export default Request;
