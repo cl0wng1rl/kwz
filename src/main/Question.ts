716dbd7e11ef6b4c8c3ec9fb44fbf65fa2dbb282
Gabriel Barker <gabrielbarker@gmail.com>
2021-06-20 16:28:35 +0100

    MOVE `Question` class

--- src/main/Question.ts
+++ src/main/Question.ts
@@ -1,25 +1,45 @@
+import { decode } from "html-entities";
+
+type OptionsObject = { [option: string]: string };
+
 class Question {
   private statement: string;
-  private correct: number;
-  private options: string[];
+  private answer: string;
+  private options: OptionsObject;
 
   constructor(statement: string, answer: string, others: string[]) {
-    this.statement = statement;
-    this.options = this.shuffle([answer, ...others]);
-    this.correct = this.options.indexOf(answer);
+    this.statement = decode(statement);
+    this.options = this.createOptionsObject([answer, ...others]);
+    this.answer = this.findAnswer(answer);
+  }
+
+  public isCorrect(answer: string): boolean {
+    return this.answer === answer;
+  }
+
+  public toString(): string {
+    let str = this.statement
+    str += "\n\n";
+    Object.entries(this.options).forEach((entry: [string, string]) => {
+      str += `${entry[0]}: ${entry[1]}\n`;
+    });
+    str += "\n";
+    return str;
+  }
+
+  private createOptionsObject(options: string[]): OptionsObject {
+    const obj: OptionsObject = {};
+    this.shuffle(options).forEach((opt, i) => (obj[String.fromCharCode(i + 65)] = opt));
+    return obj;
   }
 
   private shuffle(lst: string[]): string[] {
     lst.sort(() => 2 * Math.random() - 1);
-    return lst;
+    return lst.map((str) => decode(str));
   }
 
-  public toString(): string {
-    return {
-      statement: this.statement,
-      options: this.options,
-      correct: this.correct,
-    }.toString();
+  private findAnswer(answer: string): string {
+    return Object.keys(this.options).find((key) => this.options[key] === answer) || "";
   }
 }
 
