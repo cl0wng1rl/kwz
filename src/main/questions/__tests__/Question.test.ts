28b7d327b1df53614aabea6df9471f4cbf58de50
Gabriel Barker <gabrielbarker@gmail.com>
2022-01-09 19:30:07 +0000

    TEST the `Question` class

---
+++ src/main/questions/__tests__/Question.test.ts
@@ -0,0 +1,40 @@
+import Question from "../Question";
+// @ts-ignore
+
+beforeEach(() => {
+  jest.spyOn(global.Math, "random").mockReturnValue(0);
+});
+
+const statement: string = "statement";
+const answer: string = "answer";
+const others: string[] = ["other 1", "other 1", "other 1"];
+const correctOption = "D";
+const otherOptions = ["A", "B", "C"];
+
+const expectedString = `${statement}
+
+A: ${others[0]}
+B: ${others[1]}
+C: ${others[2]}
+D: ${answer}
+
+`;
+
+describe("Question", () => {
+  it("'isCorrect' only returns true for correct answer", async () => {
+    // Given
+    const question = new Question(statement, answer, others);
+    // When, Then
+    expect(question.isCorrect(correctOption)).toEqual(true);
+    expect(question.isCorrect(otherOptions[0])).toEqual(false);
+    expect(question.isCorrect(otherOptions[1])).toEqual(false);
+    expect(question.isCorrect(otherOptions[2])).toEqual(false);
+  });
+
+  it("'toString' returns correct string", async () => {
+    // Given
+    const question = new Question(statement, answer, others);
+    // When, Then
+    expect(question.toString()).toEqual(expectedString);
+  });
+});
