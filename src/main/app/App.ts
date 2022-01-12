3f8f872c9747dbef3261c4e1ee653474409ffa91
Gabriel Barker <gbarker@financialforce.com>
2022-01-12 22:38:09 +0000

    MAKE `App` methods static
    This allows the methods to be passed to the CLI command

--- src/main/app/App.ts
+++ src/main/app/App.ts
@@ -5,14 +5,14 @@ import CategoryReader from "../categories/CategoryReader";
 import Quiz from "./Quiz";
 
 class App {
-  public async playQuiz(args: Arguments) {
+  public static async playQuiz(args: Arguments) {
     const request = new RequestFactory().getQuestionRequest(args);
     const questions = await new QuestionFactory(request).getQuestions();
     const quiz = new Quiz(questions);
     quiz.play();
   }
 
-  public async readCategories() {
+  public static async readCategories() {
     await new CategoryReader().print();
   }
 }
